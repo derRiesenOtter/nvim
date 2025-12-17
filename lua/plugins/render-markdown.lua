ec03e781b4d0bfc5163ac02ee88e0ca88e97d4a8
derRiesenOtter <104461717+derRiesenOtter@users.noreply.github.com>
2025-12-17 22:32:36 +0100

    visual stuff

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -3,5 +3,15 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
 	opts = {
 		code = { border = "thin" },
+		heading = {
+			backgrounds = {
+				"RenderMarkdownH3Bg",
+				"RenderMarkdownH5Bg",
+				"RenderMarkdownH6Bg",
+				"RenderMarkdownH6Bg",
+				"RenderMarkdownH6Bg",
+				"RenderMarkdownH6Bg",
+			},
+		},
 	},
 }
